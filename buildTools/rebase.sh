7ef38beafee8626bf5292f384d81a6f93fbc8683
dangherve <dangherve@gmail.com>
2026-03-12 13:46:42 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/electronicarts/CnC_Remastered_Collection.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/CnC_Remastered_Collection.git
+git push --force --set-upstream origin master
